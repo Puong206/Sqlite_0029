453da41b3074e24bf34aa2fd65d7506207562186
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-24 10:58:20 +0700

    menambah implementasi UserBloc untuk memuat pengguna dari repository

--- lib/bloc/user_bloc.dart
+++ lib/bloc/user_bloc.dart
@@ -1,7 +1,20 @@
 import 'package:database_paml/bloc/user_event.dart';
 import 'package:database_paml/bloc/user_state.dart';
+import 'package:database_paml/domain/repository/user_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
-  
+  final UserRepository repository;
+
+  UserBloc(this.repository) : super(UserInitial()) {
+    on<LoadUsers>((event, emit) async {
+      emit(UserLoading());
+      try {
+        final users = await repository.getAllUsers();
+        emit(UserLoaded(users));
+      } catch (e) {
+        emit(UserError('Gagal memuat data'));
+      }
+    });
+  }
 }
