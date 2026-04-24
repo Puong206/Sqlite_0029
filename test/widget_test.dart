15c936e9acc4eb72e7897d2956343eaa72560bb5
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-24 19:22:07 +0700

    fix: perbaiki penamaan variabel userRepository untuk konsistensi

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:database_paml/data/repositories/user_repository_impl.dart';
+import 'package:database_paml/helper/database_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +15,9 @@ import 'package:database_paml/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    final dbHelper = DatabaseHelper();
+    final userRepository = UserRepositoryImpl(dbHelper);
+    await tester.pumpWidget(MyApp(repository: userRepository));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
