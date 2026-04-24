1e2f83d8f704e370e84e887a1962f97e9545db6a
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-24 10:24:32 +0700

    menambah kelas UserRepository dan metode untuk mengelola pengguna

---
+++ lib/domain/repository/user_repository.dart
@@ -0,0 +1,8 @@
+import 'package:database_paml/domain/entities/user_entity.dart';
+
+abstract class UserRepository {
+  Future<List<UserEntity>> getAllUsers();
+  Future<void> addUser(UserEntity user);
+  Future<void> updateUser(UserEntity user);
+  Future<void> deleteUser(int id);
+}
