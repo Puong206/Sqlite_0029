f540f5412c3b443e2aa1d05a99f9bd3a8e7f1671
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-24 10:34:29 +0700

    menambah konstruktor factory pada kelas DatabaseHelper

--- lib/helper/database_helper.dart
+++ lib/helper/database_helper.dart
@@ -3,4 +3,9 @@ import 'package:sqflite/sqflite.dart';
 class DatabaseHelper {
   static final DatabaseHelper _instance = DatabaseHelper._internal();
   static Database? _database;
+
+  factory DatabaseHelper() => _instance;
+  DatabaseHelper._internal();
+
+
 }
