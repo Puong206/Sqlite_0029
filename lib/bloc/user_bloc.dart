841bf5bf648dc2b15ac59cc10156aa6ad4fc8fe9
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-24 10:26:06 +0700

    menambah kelas UserBloc untuk pengelolaan status pengguna

---
+++ lib/bloc/user_bloc.dart
@@ -0,0 +1,7 @@
+import 'package:database_paml/bloc/user_event.dart';
+import 'package:database_paml/bloc/user_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  
+}
