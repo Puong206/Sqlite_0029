16cff629f98866531582626eb4dae3254186cb52
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-04-24 10:45:46 +0700

    menambah metode getAllUsers untuk mengambil semua pengguna dari database

--- lib/data/repositories/user_repository_impl.dart
+++ lib/data/repositories/user_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:database_paml/domain/entities/user_entity.dart';
 import 'package:database_paml/domain/repository/user_repository.dart';
 import 'package:database_paml/helper/database_helper.dart';
 
@@ -5,5 +6,10 @@ class UserRepositoryImpl implements UserRepository {
   final DatabaseHelper dbHelper;
   UserRepositoryImpl(this.dbHelper);
 
-  
+  @override
+  Future<List<UserEntity>> getAllUsers() async {
+    final db = await dbHelper.database;
+    final List<Map<String, dynamic>> maps = await db.query('users');
+    return maps.map((userMap) => UserEntity.fromMap(userMap)).toList(); 
+  }
 }
